5209b9487cd59630b4fdf48b7feb9a3596ba3d61
roushanatlcci <roushan@lilly.com>
2026-03-07 10:25:30 +0530

    split-feature: removing worker from main platform

--- continuum-base/src/main/kotlin/com/continuum/base/AutoConfigure.kt
+++
@@ -1,9 +0,0 @@
-package com.continuum.base
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan
-class AutoConfigure {
-}
